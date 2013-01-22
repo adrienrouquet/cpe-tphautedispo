8007ab299f7d371e65bcdb4d1eeb7cc877fc809c
loicortola <loic.ortola@cpe.fr>
2013-01-22 17:11:52 +0100

    Update datastore and user

--- cpe-tphautedispo/src/modules/datastore/DatastoreUserToolbox.java
+++ cpe-tphautedispo/src/modules/datastore/DatastoreUserToolbox.java
@@ -1,12 +1,14 @@
 package modules.datastore;
 
+import objects.User;
+
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 
 public class DatastoreUserToolbox extends DatastoreToolbox{
 
-	public Entity getEntityFromCredentials(String login, String password)
+	public User getUserFromCredentials(String login, String password)
 	{
 		Query q = new Query("User");
 		q.setFilter(new Query.FilterPredicate("login", Query.FilterOperator.EQUAL,login));
@@ -15,26 +17,37 @@ public class DatastoreUserToolbox extends DatastoreToolbox{
 		PreparedQuery pq = datastore.prepare(q);
 		
 		for(Entity rs : pq.asIterable()){
-			return rs;
+			return new User(rs);
 		}
 		return null;
 	}
-	
-//	public Entity getIsConnected(String login)
-//	{
-//		return null;
-//	}
 	
 	public boolean checkCredentials(String login, String password)
 	{
 		boolean valid = false;
 		
-		if (getEntityFromCredentials(login, password) != null)
+		if (getUserFromCredentials(login, password) != null)
 		{
 			valid = true;
-		}
-		
+		}		
 		return valid;
+	}
+	
+	public void putUserToDatastore(User user)
+	{
+		Entity userEntity = null;
+		
+		if(user.getKey() != null)
+			userEntity = new Entity("User",user.getKey());
+		else
+			userEntity = new Entity("User");
+		userEntity.setProperty("firstName", user.getFirstName());
+		userEntity.setProperty("lastName", user.getLastName());
+		userEntity.setProperty("yearOfBirth", user.getYearOfBirth());
+		userEntity.setProperty("email", user.getEmail());
+		userEntity.setProperty("login", user.getLogin());
+		userEntity.setProperty("password", user.getPassword());
+		userEntity.setProperty("rightTypeId", user.getRightTypeId());
 	}
 
 }
