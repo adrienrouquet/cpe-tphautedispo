4bb6fdaaaa7d91cbeef3b6443e424e4c7d9530ff
loicortola <loic.ortola@cpe.fr>
2013-01-22 14:52:27 +0100

    New Commit ignore classes

--- cpe-tphautedispo/src/datastore/DatastoreUserToolbox.java
+++ cpe-tphautedispo/src/datastore/DatastoreUserToolbox.java
@@ -4,14 +4,14 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 
-public class DatastoreUserToolbox {
+public class DatastoreUserToolbox extends DatastoreHandler{
 
 	@SuppressWarnings("deprecation")
 	public Entity getUserFromCredentials(String login, String password)
 	{
 		Query q = new Query("User");
-		q.addFilter("login", Query.FilterOperator.EQUAL,login);
-		q.addFilter("password", Query.FilterOperator.EQUAL,password);
+		q.setFilter(new Query.FilterPredicate("login", Query.FilterOperator.EQUAL,login));
+		q.setFilter( new Query.FilterPredicate("password", Query.FilterOperator.EQUAL,password));
 		
 		PreparedQuery pq = datastore.prepare(q);
 		
