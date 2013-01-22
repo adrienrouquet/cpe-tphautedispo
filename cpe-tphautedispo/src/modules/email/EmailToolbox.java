f6a14ab3dd4bd0397f54804285f718df4e8f811c
loicortola <loic.ortola@cpe.fr>
2013-01-22 17:27:05 +0100

    Email

---
+++ cpe-tphautedispo/src/modules/email/EmailToolbox.java
@@ -0,0 +1,100 @@
+package modules.email;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class EmailToolbox {
+	
+	protected String _emailFrom 			= "";
+	protected String _emailFromDescr 	= "";
+	protected String _emailTo 				= "";
+	protected String _emailToDescr 		= "";
+	protected String _emailSubject 		= "";
+	protected String _emailBody 			= "";
+	
+	public EmailToolbox() {
+		_emailFrom = "test@gmail.com";
+		_emailFromDescr = "Test App";
+	}
+	
+	public String getEmailFrom() {
+		return _emailFrom;
+	}
+
+	public void setEmailFrom(String _emailFrom) {
+		this._emailFrom = _emailFrom;
+	}
+
+	public String getEmailFromDescr() {
+		return _emailFromDescr;
+	}
+
+	public void setEmailFromDescr(String _emailFromDescr) {
+		this._emailFromDescr = _emailFromDescr;
+	}
+
+	public String getEmailTo() {
+		return _emailTo;
+	}
+
+	public void setEmailTo(String _emailTo) {
+		this._emailTo = _emailTo;
+	}
+
+	public String getEmailToDescr() {
+		return _emailToDescr;
+	}
+
+	public void setEmailToDescr(String _emailToDescr) {
+		this._emailToDescr = _emailToDescr;
+	}
+
+	public String getEmailSubject() {
+		return _emailSubject;
+	}
+
+	public void setEmailSubject(String _emailSubject) {
+		this._emailSubject = _emailSubject;
+	}
+
+	public String getEmailBody() {
+		return _emailBody;
+	}
+
+	public void setEmailBody(String _emailBody) {
+		this._emailBody = _emailBody;
+	}
+		
+	public void send()
+	{
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props);
+		
+		 try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(_emailFrom, _emailFromDescr));
+			
+			msg.addRecipient(Message.RecipientType.TO,	new InternetAddress(_emailTo, _emailToDescr));
+			msg.setSubject(_emailSubject);
+			msg.setText(_emailBody);
+			Transport.send(msg);
+ 
+	    }catch (AddressException e1) {
+	         // ...
+	    }catch (MessagingException e2) {
+	         // ...
+	    }catch (UnsupportedEncodingException e3) {
+			// TODO Auto-generated catch block
+		}	
+	}
+	
+	
+}
---
+++ cpe-tphautedispo/src/modules/email/EmailUserToolbox.java
@@ -0,0 +1,27 @@
+package modules.email;
+
+import objects.User;
+
+public class EmailUserToolbox extends EmailToolbox{
+	
+	public void sendSubscriptionConfirmation(User user)
+	{
+		_emailFrom 			= "subscribe@souple-airlines.com";
+		_emailFromDescr 	= "Souple Airlines";
+		_emailTo				= user.getEmail();
+		_emailToDescr		= user.getName();
+		_emailSubject		= "Thank you for your subscription";
+		_emailBody			= "Hi, and thanks for creating your account on Souple Airlines.\n\n You are now ready to use our services.\nYour password is: " + user.getPassword() + "\nPlease do not lose it.";
+		send();
+	}
+	public void sendNewPassword(User user)
+	{
+		_emailFrom 			= "password@souple-airlines.com";
+		_emailFromDescr 	= "Souple Airlines";
+		_emailTo				= user.getEmail();
+		_emailToDescr		= user.getName();
+		_emailSubject		= "Password change request";
+		_emailBody			= "Your new password is: " + user.getPassword() + "\nPlease do not lose it.";
+		send();
+	}
+}
