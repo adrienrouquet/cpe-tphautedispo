d62605c6666e8b1eb90a2c155789e69afcc2417e
loicortola <loic.ortola@cpe.fr>
2013-01-22 17:40:25 +0100

    defaultconf

---
+++ cpe-tphautedispo/src/install/DefaultConf.java
@@ -0,0 +1,26 @@
+package install;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import objects.User;
+
+import modules.datastore.DatastoreUserToolbox;
+
+public class DefaultConf  extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public DefaultConf() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException
+	{
+		System.out.println("DefaultConf: Entering doGet");
+		DatastoreUserToolbox dsut = new DatastoreUserToolbox();
+		dsut.putUserToDatastore(new User("admin", "istrator", 2000, "admin@souple-airlines.com", "admin", "password"));
+	}
+
+}
